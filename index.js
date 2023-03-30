24f96ec3ea661689a691ac69fd2ec59b4e91466e
salviega <salviega6@gmail.com>
2023-03-30 08:18:55 -0500

    [FEAT] improved in the code ⚡️

--- index.js
+++ index.js
@@ -12,36 +12,28 @@ const biorbitAbi = biorbitJson.abi
 const contract = new ethers.Contract(biorbitAddress, biorbitAbi, provider)
 
 const eventName = 'ProtectedAreaCreated'
-const callback = (id, name, photo, description, geoJson, country) => {
-	console.log('id: ', id)
-	console.log('name: ', name)
-	console.log('photo: ', photo)
-	console.log('description: ', description)
-	console.log('geoJson: ', geoJson)
-	console.log('country: ', country)
-
-	const bigNumber = ethers.BigNumber.from(id)
-	const idInteger = parseInt(bigNumber.toString(), 10)
+const callback = (_id, _name, _footprint) => {
+	const bigNumber = ethers.BigNumber.from(_id)
+	const _idInteger = parseInt(bigNumber.toString(), 10)
 
 	const object = {
-		idInteger,
-		name,
-		photo,
-		description,
-		geoJson,
-		country
+		_idInteger,
+		_name,
+		_footprint
 	}
 
-	axios
-		.post('http://localhost:5000/processing', {
-			data: object
-		})
-		.then(function (response) {
-			console.log(response)
-		})
-		.catch(function (error) {
-			console.log(error)
-		})
+	console.debug(object)
+
+	// axios
+	// 	.post('http://localhost:5000/processing', {
+	// 		data: object
+	// 	})
+	// 	.then(function (response) {
+	// 		console.log(response)
+	// 	})
+	// 	.catch(function (error) {
+	// 		console.log(error)
+	// 	})
 }
 
 contract.on(eventName, callback)
